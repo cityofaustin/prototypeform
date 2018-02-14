9f34f588dfc2275b5791d6b1003d91522c4bf48c
Mateo Clarke <code@mateoclarke.com>
2018-02-14 00:47:05 -0600

    wrap the whole thing in an IIFE bc the JS gods are watching

--- js/index.js
+++ js/index.js
@@ -1,34 +1,38 @@
-// This event handler toggles the disqualifier alert for age screening question
-$('.js-age-selection input').on('change', (e) => {
-  var hideAlert = e.target.value === "Yes";
-  $('.js-age-disqualifier-alert').toggleClass('d-none', hideAlert);
-  // TODO: Disable form if they are disqualified
-});
+(() => {
 
-// This event handler toggles the disqualifier alert for reliable transportation
-// screening question
-$('.js-transportation-selection input').on('change', (e) => {
-  var hideAlert = e.target.value === "Yes";
-  $('.js-transportation-alert').toggleClass('d-none', hideAlert);
-  // TODO: Disable form if they are disqualified
-});
+  // This event handler toggles the disqualifier alert for age screening question
+  $('.js-age-selection input').on('change', (e) => {
+    var hideAlert = e.target.value === "Yes";
+    $('.js-age-disqualifier-alert').toggleClass('d-none', hideAlert);
+    // TODO: Disable form if they are disqualified
+  });
 
-// Add and removes explanation textarea
-$('.js-add-explanation .js-add-explanation-trigger').on('click', (e) => {
-  $(e.target).closest('.js-add-explanation').find('.js-explanation-field').toggleClass('d-none');
-});
+  // This event handler toggles the disqualifier alert for reliable transportation
+  // screening question
+  $('.js-transportation-selection input').on('change', (e) => {
+    var hideAlert = e.target.value === "Yes";
+    $('.js-transportation-alert').toggleClass('d-none', hideAlert);
+    // TODO: Disable form if they are disqualified
+  });
 
-$('.js-medications-selection input').on('change', (e) => {
-  var hideAlert = e.target.value === "Yes";
-  $('.js-medications-alert').toggleClass('d-none', hideAlert);
-});
+  // Add and removes explanation textarea
+  $('.js-add-explanation .js-add-explanation-trigger').on('click', (e) => {
+    $(e.target).closest('.js-add-explanation').find('.js-explanation-field').toggleClass('d-none');
+  });
 
-$('.js-humane-selection input').on('change', (e) => {
-  var hideAlert = e.target.value === "No";
-  $('.js-humane-alert').toggleClass('d-none', hideAlert);
-});
+  $('.js-medications-selection input').on('change', (e) => {
+    var hideAlert = e.target.value === "Yes";
+    $('.js-medications-alert').toggleClass('d-none', hideAlert);
+  });
 
-$('.js-exisiting-animal input').on('change', (e) => {
-  var hideFollowup = e.target.value === "No";
-  $('.js-exisiting-animal-followup').toggleClass('d-none', hideFollowup);
-});
+  $('.js-humane-selection input').on('change', (e) => {
+    var hideAlert = e.target.value === "No";
+    $('.js-humane-alert').toggleClass('d-none', hideAlert);
+  });
+
+  $('.js-exisiting-animal input').on('change', (e) => {
+    var hideFollowup = e.target.value === "No";
+    $('.js-exisiting-animal-followup').toggleClass('d-none', hideFollowup);
+  });
+
+})();
