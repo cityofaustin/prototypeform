a252638de3ceea73bd24d20a50505c3e4ae3c343
Mateo Clarke <code@mateoclarke.com>
2018-02-14 01:32:21 -0600

    Add some structure to JS file based on module pattern

--- js/index.js
+++ js/index.js
@@ -1,38 +1,72 @@
-(() => {
+$(document).ready(() => {
 
-  // This event handler toggles the disqualifier alert for age screening question
-  $('.js-age-selection input').on('change', (e) => {
-    var hideAlert = e.target.value === "Yes";
-    $('.js-age-disqualifier-alert').toggleClass('d-none', hideAlert);
-    // TODO: Disable form if they are disqualified
-  });
+  var explanationField = (() => {
+    var init = () => {
+      // Add and removes explanation textarea
+      $('.js-add-explanation .js-add-explanation-trigger').on('click', (e) => {
+        $(e.target).closest('.js-add-explanation').find('.js-explanation-field').toggleClass('d-none');
+      });
+    }
 
-  // This event handler toggles the disqualifier alert for reliable transportation
-  // screening question
-  $('.js-transportation-selection input').on('change', (e) => {
-    var hideAlert = e.target.value === "Yes";
-    $('.js-transportation-alert').toggleClass('d-none', hideAlert);
-    // TODO: Disable form if they are disqualified
-  });
+    return { init };
+  })();
 
-  // Add and removes explanation textarea
-  $('.js-add-explanation .js-add-explanation-trigger').on('click', (e) => {
-    $(e.target).closest('.js-add-explanation').find('.js-explanation-field').toggleClass('d-none');
-  });
+  // INFO ABOUT YOURSELF PAGE
+  var aboutYourselfPage = (() => {
+    var bindEventHandlers = () => {
+      // This event handler toggles the disqualifier alert for age screening question
+      $('.js-age-selection input').on('change', (e) => {
+        var hideAlert = e.target.value === "Yes";
+        $('.js-age-disqualifier-alert').toggleClass('d-none', hideAlert);
+        // TODO: Disable form if they are disqualified
+      });
 
-  $('.js-medications-selection input').on('change', (e) => {
-    var hideAlert = e.target.value === "Yes";
-    $('.js-medications-alert').toggleClass('d-none', hideAlert);
-  });
+      // This event handler toggles the disqualifier alert for reliable transportation
+      // screening question
+      $('.js-transportation-selection input').on('change', (e) => {
+        var hideAlert = e.target.value === "Yes";
+        $('.js-transportation-alert').toggleClass('d-none', hideAlert);
+        // TODO: Disable form if they are disqualified
+      });
 
-  $('.js-humane-selection input').on('change', (e) => {
-    var hideAlert = e.target.value === "No";
-    $('.js-humane-alert').toggleClass('d-none', hideAlert);
-  });
+      $('.js-medications-selection input').on('change', (e) => {
+        var hideAlert = e.target.value === "Yes";
+        $('.js-medications-alert').toggleClass('d-none', hideAlert);
+      });
 
-  $('.js-exisiting-animal input').on('change', (e) => {
-    var hideFollowup = e.target.value === "No";
-    $('.js-exisiting-animal-followup').toggleClass('d-none', hideFollowup);
-  });
+      $('.js-humane-selection input').on('change', (e) => {
+        var hideAlert = e.target.value === "No";
+        $('.js-humane-alert').toggleClass('d-none', hideAlert);
+      });
 
-})();
+      $('.js-exisiting-animal input').on('change', (e) => {
+        var hideFollowup = e.target.value === "No";
+        $('.js-exisiting-animal-followup').toggleClass('d-none', hideFollowup);
+      });
+    }
+
+    return {
+      bindEventHandlers
+    }
+  })();
+
+  // INFO ABOUT YOUR AVAILABILITY
+  var aboutAvailabilityPage = (() => {
+    var bindEventHandlers = () => {
+      $('.js-left-alone-selection').on('change', (e) => {
+        $('.js-left-alone-alert').removeClass('d-none');
+
+        var alertText = $(e.target).siblings('.js-selection-alert-text').text();
+        $('.js-left-alone-alert').text(alertText);
+      })
+    }
+
+    return {
+      bindEventHandlers
+    }
+  })();
+
+  explanationField.init();
+  aboutYourselfPage.bindEventHandlers();
+  aboutAvailabilityPage.bindEventHandlers();
+});
