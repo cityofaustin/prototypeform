5a87b35fb3b50db1b92f9bd4ee329eaf22c571a7
Mateo Clarke <code@mateoclarke.com>
2018-02-14 00:27:17 -0600

    finish up yourself page

--- js/index.js
+++ js/index.js
@@ -11,19 +11,24 @@ $('.js-transportation-selection input').on('change', (e) => {
   var hideAlert = e.target.value === "Yes";
   $('.js-transportation-alert').toggleClass('d-none', hideAlert);
   // TODO: Disable form if they are disqualified
-})
+});
 
 // Add and removes explanation textarea
 $('.js-add-explanation .js-add-explanation-trigger').on('click', (e) => {
   $(e.target).closest('.js-add-explanation').find('.js-explanation-field').toggleClass('d-none');
-})
+});
 
 $('.js-medications-selection input').on('change', (e) => {
   var hideAlert = e.target.value === "Yes";
   $('.js-medications-alert').toggleClass('d-none', hideAlert);
-})
+});
 
 $('.js-humane-selection input').on('change', (e) => {
   var hideAlert = e.target.value === "No";
   $('.js-humane-alert').toggleClass('d-none', hideAlert);
-})
+});
+
+$('.js-exisiting-animal input').on('change', (e) => {
+  var hideFollowup = e.target.value === "No";
+  $('.js-exisiting-animal-followup').toggleClass('d-none', hideFollowup);
+});
